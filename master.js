91b66217aaa1e04e4b858dbdaf41928f4dfce959
CosmicCandi <kalea.wolff@gmail.com>
2017-03-26 20:59:51 -0400

    Finishes explorer mode.

--- master.js
+++ master.js
@@ -7,6 +7,22 @@ $(document).ready(function(){
   tagged_notes_url = api_root + "notes/tag/"
 
 
+  function reset_form(form_id){
+    $(form_id)[0].reset()
+  }
+
+    //Create a post
+    $('#note_create').on('submit', function(ev){
+      ev.preventDefault()
+      $.post(api_root + 'notes', $(this).serialize())
+      .done(function(note){
+        $('#note_content').prepend(
+          note_display(note.note)
+        )
+        reset_form('#note_create')
+      })
+    })
+
     //Hide the note creation form by clicking the 'Meister a Note' Button
     $('#show_note_form_button').on('click', function(){
     $('.create_a_note').slideToggle(400)
@@ -20,11 +36,20 @@ $(document).ready(function(){
       populate_tagged_notes(tag_name)
   })
 
+  //Replace a non-user's username with anonymous
+  function replace_null_username(note){
+    if(note.user === null){
+      return "Anonymous"
+    }else{
+      return note.user.username
+    }
+  }
+
   function note_display(note){
     return `
-      <div class="media" id="note-${note.id}">
+      <div class="media" id="${note.id}">
         <div class="media-left media-middle">
-          <p class="note-username">${note.user.username}</p>
+          <p class="note-username">${replace_null_username(note)}</p>
         </div>
         <div class="media-body">
         <h4 class="media-heading text-center">${note.title}</h4>
@@ -53,6 +78,12 @@ $(document).ready(function(){
             note_display(note)
           )
         })
+        //Open a modal window when URL includes #note.id
+        if(window.location.hash){
+          $('#modal_win .modal-body').html($(location.hash).html())
+          $('#modal_win').modal('show')
+        }
+
       })
   }
 
